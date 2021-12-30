7ca197736f74f68afbfe5320c101d4f0abc6c2a1
webdesign4alll <190048@tkmce.ac.in>
2021-12-30 12:19:58 +0530

    added 29 recorded

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 29, 2021 18:30:00").getTime();
+var countDownDate = new Date("Dec 30, 2021 18:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
